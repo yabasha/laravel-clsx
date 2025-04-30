47874ac945313ee5720fdb16844faf6114d61399
Bashar Ayyash <bayyash.c@neoleap.com.sa>
2025-04-30 11:17:09 +0300

    Add clsx_with_error helper function to manage error classes in validation

--- src/helpers.php
+++ src/helpers.php
@@ -13,3 +13,23 @@ if (! function_exists('clsx')) {
         return Clsx::make(...$args);
     }
 }
+
+if (! function_exists('clsx_with_error')) {
+    /**
+     * Join class names and add an error class if the validation errors contain the given key.
+     *
+     * @param string $key
+     * @param array $errors
+     * @param string $errorClass
+     * @param mixed ...$args
+     * @return string
+     */
+    function clsx_with_error(string $key, $errors, string $errorClass = 'is-invalid', ...$args): string
+    {
+        $hasError = ($errors instanceof \Illuminate\Support\MessageBag)
+            ? $errors->has($key)
+            : (is_array($errors) ? !empty($errors[$key]) : false);
+        $args[] = [$errorClass => $hasError];
+        return clsx(...$args);
+    }
+}
